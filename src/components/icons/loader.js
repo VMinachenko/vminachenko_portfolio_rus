6889cf4237d891051f7a2e00191e186d11bfb480
WhiskeyAGo-Go <v.minachenko@gmail.com>
2021-08-01 11:59:17 +0300

    initial commit

---
+++ src/components/icons/loader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const IconLoader = () => (
+  <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="261.021"
+  height="298.536"
+  viewBox="0 0 261.021 298.536"
+>
+  <g transform="translate(-238.695 -107.379)">
+    <text
+      transform="translate(293 324)"
+      fill="#63ffda"
+      font-size="200"
+      font-family="Segoe UI"
+      font-weight="600"
+    >
+      <tspan x="13" y="0">
+        V
+      </tspan>
+    </text>
+    <path
+      d="M435.651,850.174l126.064-68.158V637.388L437.036,563.135,310.695,632.124V776.474Z"
+      transform="translate(-67 -450)"
+      fill="none"
+      stroke="#63ffda"
+      stroke-width="10"
+    />
+  </g>
+</svg>
+
+);
+
+export default IconLoader;
---
+++ src/components/sections/about.js
@@ -0,0 +1,173 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = ['Excel (XLStat, @Risk)', 'SQL', 'Tableau', 'RapidMiner', 'Python (pandas, numpy, matplotlib, sklearn) ', 'LINGO Линейное Программирование', ];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">Обо мне</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+            Привет! Меня зовут Вячеслав, но вы можете называть меня Слава. Я целеустремленный аналитик данных начального уровня
+              с творческим мышлением и страстью к обучению и инновациям.
+              Во время учебы я фокусировался на логистике и аналитике цепочек поставок, чтобы освоить
+              способность переводить бизнес-требования в технические решения в системах цепочек поставок. 
+              После академических проектов, связанных с интеллектуальным анализом данных и машинным
+              обучением, я считаю, что эффективное использование этих технологий является одним из
+              наиболее важных факторов для устранения асимметрии информации и построения крайне эффективных,
+              прозрачных и устойчивых отношений между всеми вовлеченными сторонами, от
+              производителей до клиентов. Учитывая недавние события и последовавший за ними бум в
+              индустрии электронной коммерции, я серьезно мотивирован получить необходимые навыки и начать свою
+              профессиональную деятельность в области электронной коммерции и цифровых цепочек поставок. 
+            </p>
+            <p>Вот несколько технологий, с которыми я работал:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
